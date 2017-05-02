9492bcc45442b7821cb8b320c5f0371021197ab1
tkapa <1005732@Student.sae.edu.au>
2017-05-02 22:39:09 +1000

    Simple Tool Created
    I have created a tool for the Unity editor that allows users to change the material of all objects assigned a particular tag.

---
+++ Assets/Editor/SelectAllOfTag.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SelectAllOfTag : ScriptableWizard {
+
+    [Tooltip("Insert the tag you are looking for")]
+    public string searchTag = "Your Tag Here";
+    public Material NewMaterial;
+
+	[MenuItem("My Tools/Select All of Tag...")]
+    static void SelectAllOfTagWizard()
+    {
+        ScriptableWizard.DisplayWizard<SelectAllOfTag>("Change all tag materials...", "Change Materials");
+    }
+
+    void OnWizardCreate()
+    {
+        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag(searchTag);
+
+        foreach(GameObject o in gameObjects)
+        {
+            o.GetComponent<Renderer>().material = NewMaterial;
+        }
+    }
+
+    void OnWizardUpdate()
+    {
+        if (searchTag == "Your Tag Here" || NewMaterial == null)
+            isValid = false;
+        else
+            isValid = true;
+    }
+}
